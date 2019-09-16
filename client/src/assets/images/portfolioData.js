c075f03fb1a04b62e8d64c927d481af9483562b8
Cara Sunberg <cnsunberg@gmail.com>
2019-09-16 12:01:51 -0400

    Added all items to portfolio component in portfolioData array

--- client/src/assets/images/portfolioData.js
+++ client/src/assets/images/portfolioData.js
@@ -8,9 +8,51 @@ module.exports = [
     },
     {
         id: 2,
-        image: require("./harrypotter.png"),
+        image: require("./harrypotter2.png"),
         link: "https://harrypotter-csd.herokuapp.com",
-        title: "Harry Potter Story",
+        title: "A Harry Potter Story",
         alt: "Harry Potter Story screenshot"
+    },
+    {
+        id: 3,
+        image: require("./trivia.png"),
+        link: "https://cstkpk.github.io/TriviaGame/",
+        title: "Parks & Recreation Trivia Game",
+        alt: "Trivia game screenshot"
+    },
+    {
+        id: 4,
+        image: require("./goat4.gif"),
+        link: "https://cstkpk.github.io/GifTastic/",
+        title: "Giftastic",
+        alt: "Gif of goat playing guitar"
+    },
+    {
+        id: 5,
+        image: require("./bamazon.png"),
+        link: "https://github.com/cstkpk/bamazon/",
+        title: "Bamazon",
+        alt: "Bamazon CLI screenshot"
+    },
+    {
+        id: 6,
+        image: require("./liribot.png"),
+        link: "https://github.com/cstkpk/liri-node-app",
+        title: "LIRI Bot",
+        alt: "LIRI Bot CLI screenshot"
+    },
+    {
+        id: 7,
+        image: require("./psychic.png"),
+        link: "https://cstkpk.github.io/Psychic-Game/",
+        title: "The Psychic Game",
+        alt: "Pyschic Game screenshot"
+    },
+    {
+        id: 8,
+        image: require("./crystals.png"),
+        link: "https://cstkpk.github.io/unit-4-game/",
+        title: "Crystal Collector",
+        alt: "Crystal Collector screenshot"
     }
 ]
